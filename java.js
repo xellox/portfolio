e65f672a54fc12bff6b8f35e069ccff108b989e6
xellox <korpasdarius911@gmail.com>
2023-07-16 11:54:26 +0300

    new file:   images/blob.svg 	new file:   images/logo-color.png 	new file:   images/logo-no-background.png 	new file:   images/trimLogo.png 	new file:   images/trimLogo2.png 	modified:   index.html 	modified:   java.js 	modified:   style.css

--- java.js
+++ java.js
@@ -7,9 +7,13 @@ const popUpEmail = document.querySelector(".email-sent")
 
 function submitDone(){
     form.style.display = "none";
-    popUpEmail.style.display = "block"
+    popUpEmail.style.display = "flex"
 }
 
+popUpEmail.addEventListener("click", () => {
+    form.style.display = "block";
+    popUpEmail.style.display = "none"
+})
 function sendEmail(){
     let ebody = `
     <b>Name: </b>${fname.value}
@@ -32,3 +36,102 @@ function sendEmail(){
     );
 
     }
+
+
+
+
+    // Vanilla JavaScript solution
+document.addEventListener('DOMContentLoaded', function() {
+    var animateOnScrollElements = document.querySelectorAll('.animate-on-scroll-right');
+  
+    function isElementInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+        rect.top >= -180 &&
+        rect.left >= -180 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function animateOnScroll() {
+      for (var i = 0; i < animateOnScrollElements.length; i++) {
+        var element = animateOnScrollElements[i];
+        if (isElementInViewport(element)) {
+          element.classList.add('show');
+        } else{
+            element.classList.remove('show')
+        }
+      }
+    }
+  
+    animateOnScroll();
+    window.addEventListener('scroll', animateOnScroll);
+  });
+    
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var animateOnScrollElements = document.querySelectorAll('.animate-on-scroll-left');
+    
+    function isElementInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+        rect.top >= -180 &&
+        rect.left >= -180 &&
+        rect.bottom <= ((window.innerHeight )|| document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function animateOnScroll() {
+      for (var i = 0; i < animateOnScrollElements.length; i++) {
+        var element = animateOnScrollElements[i];
+        if (isElementInViewport(element)) {
+          element.classList.add('show');
+        } else {
+            element.classList.remove('show')
+            
+           }
+      }
+    }
+  
+    animateOnScroll();
+    window.addEventListener('scroll', animateOnScroll);
+  });
+    
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var animateOnScrollElements = document.querySelectorAll('.animate-on-scroll-scale');
+  
+    function isElementInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+        rect.top >= -180 &&
+        rect.left >= -180 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function animateOnScroll() {
+      for (var i = 0; i < animateOnScrollElements.length; i++) {
+        var element = animateOnScrollElements[i];
+        if (isElementInViewport(element)) {
+          element.classList.add('show');
+          links.classList.toggle(".open")
+        } else {
+            element.classList.remove('show')
+            
+        }
+      }
+    }
+  
+    animateOnScroll();
+    window.addEventListener('scroll', animateOnScroll);
+  });
+    
