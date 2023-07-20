a857f9a1977b698b6ea9e9cef4b8887f94cb7166
xellox <korpasdarius911@gmail.com>
2023-07-20 12:05:35 +0300

    modified:   index.html 	modified:   java.js 	modified:   style.css

--- java.js
+++ java.js
@@ -5,15 +5,21 @@ const message = document.getElementById('message');
 const form = document.querySelector("form");
 const popUpEmail = document.querySelector(".email-sent")
 
+
+
+
 function submitDone(){
     form.style.display = "none";
     popUpEmail.style.display = "flex"
 }
 
+
 popUpEmail.addEventListener("click", () => {
     form.style.display = "block";
     popUpEmail.style.display = "none"
 })
+
+
 function sendEmail(){
     let ebody = `
     <b>Name: </b>${fname.value}
@@ -69,3 +75,28 @@ function sendEmail(){
 
     // Add an event listener to the page load event
     window.addEventListener('load', scrollToTop);
+
+    const hamburger = document.querySelector(".hamburger")
+    const hamburgerNav = document.querySelector(".hamburger-nav")
+
+    hamburger.addEventListener("click",  () => {
+      hamburger.classList.toggle("active");
+      hamburgerNav.classList.toggle("active")
+    })
+
+    document.querySelectorAll(".hamburger-link").forEach(n => 
+      n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        hamburgerNav.classList.remove("active")
+      }))
+
+
+
+window.addEventListener("resize", adjustNavbarDisplay);
+
+function adjustNavbarDisplay() {
+  if (window.innerWidth > 768) {
+    hamburger.classList.remove("active");
+    hamburgerNav.classList.remove("active")
+  }
+}
